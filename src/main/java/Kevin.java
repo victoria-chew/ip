20ffe4b115d4b0c5b5153447c43e254a0fb61858
victoria-chew <vky.chew@gmail.com>
2026-01-20 21:25:07 +0800

    Level-0: rename chatbot and greeting

---
+++ src/main/java/Kevin.java
@@ -0,0 +1,7 @@
+public class Kevin {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Kevin");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
