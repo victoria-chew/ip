e94236069ed2efc0be713d99c43878c59c8a639e
victoria-chew <vky.chew@gmail.com>
2026-01-21 00:21:07 +0800

    Level-2: add and list

--- src/main/java/Kevin.java
+++ src/main/java/Kevin.java
@@ -4,11 +4,14 @@ public class Kevin {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String[] items = new String[100]; // list of things
+        int count = 0;                    // number of items
+
         //hello
         System.out.println("Hello! I'm Kevin");
         System.out.println("What can I do for you?");
 
-        //echo
+        //echo + list/add
         while (true) {
             String input = scanner.nextLine();
 
@@ -16,8 +19,16 @@ public class Kevin {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             }
-
-            System.out.println(input);
+            else if (input.equals("list")) {
+                for (int i = 0; i < count; i++) {
+                    System.out.println((i + 1) + ". " + items[i]);
+                }
+            }
+            else {
+                items[count] = input;
+                count++;
+                System.out.println("added: " + input);
+            }
         }
     }
 }
