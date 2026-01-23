a737fc018ca702d1a6c46d4de169cba4185986a2
victoria-chew <vky.chew@gmail.com>
2026-01-23 13:32:08 +0800

    Level-3: task class (mark and unmark)

--- src/main/java/Kevin.java
+++ src/main/java/Kevin.java
@@ -4,14 +4,14 @@ public class Kevin {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String[] items = new String[100]; // list of things
-        int count = 0;                    // number of items
+        Task[] tasks = new Task[100]; //list of tasks
+        int count = 0;
 
         //hello
         System.out.println("Hello! I'm Kevin");
         System.out.println("What can I do for you?");
 
-        //echo + list/add
+        //echo + list/add + mark + unmark
         while (true) {
             String input = scanner.nextLine();
 
@@ -21,14 +21,33 @@ public class Kevin {
             }
             else if (input.equals("list")) {
                 for (int i = 0; i < count; i++) {
-                    System.out.println((i + 1) + ". " + items[i]);
+                    System.out.println((i + 1) + ". " + tasks[i]);
                 }
             }
+            else if (input.startsWith("mark ")) {
+                int taskNumber = Integer.parseInt(input.split(" ")[1]);
+                int index = taskNumber - 1;
+
+                tasks[index].markDone();
+
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(tasks[index]);
+            }
+            else if (input.startsWith("unmark ")) {
+                int taskNumber = Integer.parseInt(input.split(" ")[1]);
+                int index = taskNumber - 1;
+
+                tasks[index].markUndone();
+
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println(tasks[index]);
+            }
             else {
-                items[count] = input;
+                tasks[count] = new Task(input);
                 count++;
                 System.out.println("added: " + input);
             }
         }
     }
 }
+
